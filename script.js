8009dafd905eb3ddb1249ec0db9c4bfaac885525
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-03-17 15:43:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const userResponse = confirm("Do you want to proceed?");
+
+if (userResponse) {
+    alert("You clicked OK. Proceeding...");
+} else {
+    alert("You clicked Cancel. Exiting...");
+}
